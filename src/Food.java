67135c369bfe10f48fbc7b6c211d3c2b0622776d
sanan90 <san-an@bk.ru>
2020-08-08 17:43:49 +0300

    HW-7

---
+++ src/Food.java
@@ -0,0 +1,10 @@
+public class Food {
+
+    int food;
+
+    public void addFood(int food) {
+        Plate food1 = new Plate();
+
+    }
+
+}
---
+++ src/Plate.java
@@ -0,0 +1,13 @@
+public class Plate extends Main {
+
+    int food;
+
+    public void addFood(int food) {
+        this.food += food;
+    }
+
+    public void decreaseFood (int appetite) {
+        this.food -= appetite;
+    }
+
+}
